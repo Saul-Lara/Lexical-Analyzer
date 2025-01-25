a1d8e8cb01ed45dfc91abcf15ac1df3ddb6b66d8
Saul Lara <hl.saul.01@gmail.com>
2025-01-24 20:03:52 -0600

    👌 IMPROVE: Add line attribute

--- src/Lexer.py
+++ src/Lexer.py
@@ -6,15 +6,18 @@ class Token:
 
     Attributes:
         type: A string indicating the token type.
-        value: A string with the value of the token
+        value: A string with the value of the token.
+        line: A string with the line number of the token.
     """
 
-    def __init__(self, type: str, value: str):
+    def __init__(self, type: str, value: str, line: str):
         """Initializes a new Token instance with a type and value.
 
         Args:
             type: Define the 'TokenType' corresponding to the type of the 'Token'.
             value: Define the 'String' value of the token. The actual characters of the lexeme.
+            line: Define the line number where the token was encountered.
         """
         self.type = type
         self.value = value
+        self.line = line
